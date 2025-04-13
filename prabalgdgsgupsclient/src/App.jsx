911bc9b2cce0a2ffe75e74c1e93d5c57e06a53fc
om-ghante <omghante.official@gmail.com>
2025-04-14 00:47:01 +0530

    Console Manipulation Restriction Updated

--- prabalgdgsgupsclient/src/App.jsx
+++ prabalgdgsgupsclient/src/App.jsx
@@ -1,5 +1,5 @@
-// App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/Auth/ProtectedRoute';
 import Login from './components/Auth/Login';
@@ -8,6 +8,16 @@ import Dashboard from './pages/Dashboard';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    console.log(
+      "%cStop!",
+      "color: red; font-size: 50px; font-weight: bold;"
+    );
+    console.log(
+      "%cThis is a browser feature intended for developers...",
+      "font-size: 16px;"
+    );
+  }, []);
   return (
     <Router>
       <AuthProvider>
